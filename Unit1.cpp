db287244a7741f22e821da088e2817ed894a8bfc
KateStash <33021336+KateStash@users.noreply.github.com>
2017-10-23 13:30:00 +0300

    step2

--- Unit1.cpp
+++ Unit1.cpp
@@ -1,1 +1,33 @@
+    #include <iostream>
+#include <conio.h>
+#include <math.h>
+#include <vcl.h>
+#pragma hdrstop
 
+
+//---------------------------------------------------------------------------
+#pragma argsused
+int main(int argc, char* argv[])
+{
+	int x;
+	float y;
+	std::
+		cin >> x;
+	if (!cin || x < -1 || x > 20)
+	{
+		cout << "Error!" << endl;
+		getch();
+	}
+
+
+				else
+					y = pow(x, 4);
+		cout << "Znachenie funkcii =" << y << endl;
+
+
+
+		getch();
+		return 0;
+	}
+}
+//-----------------
